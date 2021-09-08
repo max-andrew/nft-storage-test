2c225184402331dae73ae076f30956ed863754db
Max Andrew <maxsandrew@gmail.com>
2021-09-08 09:42:15 -0400

    Initial commit

---
+++ .next/static/xH9t3Qib_-ySoDKeq7iaF/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/346-7c01dd41bd7fa599354b.js","static/chunks/pages/index-9e9a1caa80b3c51d0d18.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+import { NFTStorage, File, Blob } from 'nft.storage' 
+
+export default function Test() {
+	return (
+		<>
+			<Head>
+				<title>NFT.Storage Test</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<main>
+				<div>
+					<p>hello</p>
+				</div>
+			</main>
+		</>
+	)
+}
